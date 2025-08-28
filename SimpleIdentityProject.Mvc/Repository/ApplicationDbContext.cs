8856aefd7d74e876241520207e7ff35ffaf93d92
ArnoHuygelier <arno2003.huygelier1@gmail.com>
2025-08-28 17:36:08 +0200

    Fixed some small errors; Identity is now working.

--- SimpleIdentityProject.Mvc/Repository/ApplicationDbContext.cs
+++ SimpleIdentityProject.Mvc/Repository/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SimpleIdentityProject.Mvc.Models;
 
 namespace SimpleIdentityProject.Mvc.Repository
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {

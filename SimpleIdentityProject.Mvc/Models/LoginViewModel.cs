6d5b1e4571ca3287fc082a34557f477a477395d7
ArnoHuygelier <arno2003.huygelier1@gmail.com>
2025-08-28 17:27:34 +0200

    Added account controller and views for login and register.

---
+++ SimpleIdentityProject.Mvc/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleIdentityProject.Mvc.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
